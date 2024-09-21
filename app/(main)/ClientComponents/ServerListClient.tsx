09c98a6e3aa981860e146b7d88f7b2cd5e0c7f1a
Paul Chan <0571chz@gmail.com>
2024-09-21 16:09:37 +0800

    Update ServerListClient.tsx

--- app/(main)/ClientComponents/ServerListClient.tsx
+++ app/(main)/ClientComponents/ServerListClient.tsx
@@ -18,7 +18,7 @@ export default function ServerListClient() {
     }
     if (a.display_index) return -1;
     if (b.display_index) return 1;
-    # return a.id - b.id;
+    // return a.id - b.id;
     return a.name - b.name;
   });
 
